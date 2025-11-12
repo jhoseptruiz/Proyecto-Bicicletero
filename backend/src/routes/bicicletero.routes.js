514fb3e8d4bbeed76029cb8360d2c426890cd4f5
Milencka Arriagada <milencka.arriagada2201@alumnos.ubiobio.cl>
2025-11-12 12:44:15 -0300

    Edicion de bicicleteros

--- backend/src/routes/bicicletero.routes.js
+++ backend/src/routes/bicicletero.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import { getBicicleteros, postBicicleteros } from "../controllers/bicicletero.controller.js";
+import { getBicicleteros, postBicicleteros, putBicicleteros } from "../controllers/bicicletero.controller.js";
 import { verificarToken, checkRol } from "../middlewares/auth.middleware.js";
 
 const router = Router();
@@ -7,5 +7,6 @@ router.use(verificarToken, checkRol(['admin']));
 
 router.get("/", getBicicleteros);
 router.post("/", postBicicleteros);
+router.put("/:id", putBicicleteros);
 
 export default router;
